96a7104d2c9330caef8b7b352aaa11ce637379c5
Nito Crespi <63932237+Nito-Crespi@users.noreply.github.com>
2026-05-14 16:09:56 -0300

    Refactor Nito component to use anchor tag for portfolio link

--- src/components/Nito.tsx
+++ src/components/Nito.tsx
@@ -1,17 +1,38 @@
+// import "./Nito.css";
+
+// const Nito = () => {
+//   const handleRedirect = () => {
+//     window.open("https://nito-crespi.github.io/portfolio/", "_blank");
+//   };
+
+//   return (
+//     <div className="nito-crespi">
+//       <p className="nito-crespi__text">
+//         Desarrollador:{" "}
+//         <span className="nito-crespi__link" onClick={handleRedirect}>
+//           @Nito.Crespi
+//         </span>
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default Nito;
 import "./Nito.css";
 
 const Nito = () => {
-  const handleRedirect = () => {
-    window.open("https://nito-crespi.github.io/portfolio/", "_blank");
-  };
-
   return (
     <div className="nito-crespi">
       <p className="nito-crespi__text">
         Desarrollador:{" "}
-        <span className="nito-crespi__link" onClick={handleRedirect}>
+        <a
+          className="nito-crespi__link"
+          href="https://nito-crespi.github.io/portfolio/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           @Nito.Crespi
-        </span>
+        </a>
       </p>
     </div>
   );
